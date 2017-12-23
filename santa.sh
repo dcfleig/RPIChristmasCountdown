9cc313a9c15c6394e51f299f1f89342f98a265ad
David Fleig <david@fleigs.com>
2017-12-23 15:47:51 -0500

    Testing git

--- santa.sh
+++ santa.sh
@@ -11,7 +11,6 @@ case "$1" in
     echo "Starting script santa "
     sudo /home/pi/RpiChristmasCountdown/countdown &
     #sudo /home/pi/rpi-rgb-led-matrix/examples-api-use/clock-dcf --led-chain=4 --led-rows=32 -C 0,0,255 -O 255,255,255 -b20 &
-
     ;;
   stop)
     echo "Stopping script santa"
